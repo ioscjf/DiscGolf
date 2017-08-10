d278eef0496b3acbc8936dc44f75254f7b1f7614
Connor Fitzpatrick <fitzco01@luther.edu>
2017-08-10 03:15:57 -0500

    Starting to add the camera for course submissions

---
+++ DiscGolf/AddHoleViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AddHoleViewController.swift
+//  DiscGolf
+//
+//  Created by Connor Fitzpatrick on 8/10/17.
+//  Copyright © 2017 Connor Fitzpatrick. All rights reserved.
+//
+
+import UIKit
+
+class AddHoleViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var instructions: UILabel!
+    @IBOutlet weak var selectedImage: UIImageView!
+    @IBOutlet weak var holeLetter: UILabel!
+    @IBOutlet weak var holeLetterPicker: UIPickerView!
+    @IBOutlet weak var par: UITextField!
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            var imagePicker = UIImagePickerController()
+            imagePicker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alertController = UIAlertController(title: "There seems to be an issue with your camera", message: "Please check that the camera is working on your device, and that DiscFinder has permission to use it in your settings.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let no = UIAlertAction(title: "No", style: UIAlertActionStyle.default) {
+                (result : UIAlertAction) -> Void in
+            }
+            
+            alertController.addAction(no)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func submit(_ sender: UIButton) {
+    }
+    
+    // MARK: - Overrides
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
